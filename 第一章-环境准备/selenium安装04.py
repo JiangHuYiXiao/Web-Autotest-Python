4ae5dee9951f36ecdfb1d6a1e7f1ef734c7224c1
JiangHuYiXiao <1163270704@qq.com>
2020-04-08 09:09:38 +0800

    selenium安装04

---
+++ 第一章-环境准备/selenium安装04.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/4/8 9:00
+# @Software       : Web-Autotest-Python
+# @Python_verison : 3.7
+
+# 1、在线安装
+    # 1.1通过cmd命令窗口的pip进行安装
+        # 进入pthon/script目录执行pip命令即可安装selenium：pip install -u selenium
+    # 1.2通过pycharm安装
+        # File | Settings | Project: Web-Autotest-Python | Project Interpreter解释器下搜索selenium安装
+
+# 2、下载安装包进行安装
+   # 2.1网上下载selenium安装包后进行安装
