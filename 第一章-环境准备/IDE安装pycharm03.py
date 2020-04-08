757553f1bc591d6c10b0f336d81059c78373297b
JiangHuYiXiao <1163270704@qq.com>
2020-04-08 08:51:42 +0800

    IDE安装pycharm03

---
+++ 第一章-环境准备/IDE安装pycharm03.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/4/8 8:40
+# @Software       : Web-Autotest-Python
+# @Python_verison : 3.7
+
+# 1、下载pycharm
+    # https://www.jetbrains.com
+
+# 2、安装
+    # 下一步
+
+# 3、破解
+#     百度云下的Pycharm破解目录有对应的破解补丁
+
+# 4、解释器选择
+    # 选择本机的解释器地址，需要选择新建一个虚拟解释器
+    # C:\Users\Administrator\AppData\Local\Programs\Python\Python37\python.exe
