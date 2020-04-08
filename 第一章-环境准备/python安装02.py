3ef185c26c35f7602932b76133f0d57cb42ca8dc
JiangHuYiXiao <1163270704@qq.com>
2020-04-08 08:37:03 +0800

    Initial commit

---
+++ 第一章-环境准备/python安装02.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/4/8 8:17
+# @Software       : Web-Autotest-Python
+# @Python_verison : 3.7
+
+# 1、下载
+    # 下载官网地址：https://www.python.org/  尽量下载3.0版本的
+
+# 2、安装
+    # 一直下一步
+
+# 3、配置环境变量
+    # 路径：计算机右键属性--->高级系统配置--->环境变量--->PATH
+    # C:\Users\Administrator\AppData\Local\Programs\Python\Python37\Scripts\;C:\Users\Administrator\AppData\Local\Programs\Python\Python37\
+# 4、环境验证
+    # 在cmd窗口输入python不会报错，返回对应的Python版本
---
+++ 第一章-环境准备/目录01.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/4/8 8:09
+# @Software       : Web-Autotest-Python
+# @Python_verison : 3.7
+
+'''
+1、环境准备
+2、元素定位
+3、常用webdriverAPI操作
+4、单元测试框架：unittest
+5、webdriverAPI高级
+6、数据驱动
+7、持续集成
+8、自定义测试框架
+'''
