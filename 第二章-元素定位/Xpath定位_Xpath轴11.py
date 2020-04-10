ff9db8e8f2eea52fc39607318ed14a52237bf5b0
JiangHuYiXiao <1163270704@qq.com>
2020-04-10 11:34:31 +0800

    Xpath定位_Xpath轴11

--- 第二章-元素定位/Xpath定位_Xpath轴11.py
+++ 第二章-元素定位/Xpath定位_Xpath轴11.py
@@ -6,10 +6,14 @@
 
 # Xpath轴（Axes）定位：
 # 1、parent选择当前节点的上一层父节点
-    # img[@alt='div1']/parent::div                  div1的上一层div
+    # //img[@alt='div1']/parent::div                  div1的上一层div
 
 # 2、child选择当前节点下层的所有子节点
-    # img[@alt='div1']/child::div                  div1的下层的所有div
+    # //img[@alt='div1']/child::div                  div1的下层的所有div
 
 # 3、ancestor选择当前节点的所有上层节点
-    # img[@alt='div1']/ancestor::div                  div1的上一层div
+    # //img[@alt='div1']/ancestor::div                  div1的上一层div
+
+# 4、通过text()函数定位
+    # //a[text()="百度一下"]
+    # //a[.="百度一下"]
