8e7a01b33b4243489332a58f90e64e4ff9e7b6f5
JiangHuYiXiao <1163270704@qq.com>
2020-04-12 07:29:14 +0800

    常用API操作

--- 第三章-常用WebAPI操作/常用API操作_键盘鼠标操作_输入_热键06.py
+++ 第三章-常用WebAPI操作/常用API操作_键盘鼠标操作_输入_热键06.py
@@ -4,13 +4,28 @@
 # @Software       : Web-Autotest-Python
 # @Python_verison : 3.7
 
-# 1、send_keys()
+# 1、send_keys(内容或者键盘输入)
+# 2、ctrl+A,ctrl+C,ctrl+V，ctrl+X等等热键组合
 
+# 示例1：关1键的输入
 from selenium import webdriver
 from time import sleep
-from selenium.webdriver.common import keys
+from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome()
 driver.get('http://www.baidu.com')
-driver.find_element_by_xpath('').send_keys('')
+# 输入框输入内容
+driver.find_element_by_xpath('//input[@id="kw"]').send_keys('鬼吹灯')
+sleep(2)
+# 输入框全选操作ctrl+A
+driver.find_element_by_xpath('//input[@id="kw"]').send_keys(Keys.CONTROL,'a')
+sleep(2)
+# 输入框剪切操作ctrl+X
+driver.find_element_by_xpath('//input[@id="kw"]').send_keys(Keys.CONTROL,'x')
+sleep(2)
+# 输入框全选操作ctrl+V
+driver.find_element_by_xpath('//input[@id="kw"]').send_keys(Keys.CONTROL,'v')
+sleep(2)
+driver.find_element_by_xpath('//input[@id="su"]').click()
+sleep(2)
 driver.quit()
 
