cbdcb6a1aa0d12b2375a917aa82a04ec62afd253
JiangHuYiXiao <1163270704@qq.com>
2020-04-15 14:59:30 +0800

    unittest框架

---
+++ 第四章-unittest框架/unittest框架_自动生成HTML测试报告09/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/4/15 9:05
+# @Software       : Web-Autotest-Python
+# @Python_verison : 3.7
---
+++ 第四章-unittest框架/unittest框架_自动生成HTML测试报告09/run.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/4/15 9:06
+# @Software       : Web-Autotest-Python
+# @Python_verison : 3.7
+
+# 三种测试报告：HTMLTestRunner,HTMLTestRunner_cn,HTMLTestRunner_PY3
+
+import unittest,HTMLTestRunner,HTMLTestRunner_cn,HTMLTestRunner_PY3
+
+dir = './case/'
+suite = unittest.defaultTestLoader.discover(start_dir=dir,pattern='Test*.py')
+if __name__ == '__main__':
+    # file = open('./result.html',mode='wb')
+    # HTMLTestRunner
+    # runner = HTMLTestRunner.HTMLTestRunner(stream=file,title='百度搜索测试报告',description='含有三条用例，测试中文、英文、拼音的三个的搜索结果')
+
+    # HTMLTestRunner_cn
+    # file = open('./result1.html',mode='wb')
+    # runner = HTMLTestRunner_cn.HTMLTestRunner(stream=file,title='百度搜索测试报告',description='含有三条用例，测试中文、英文、拼音的三个的搜索结果')
+    # HTMLTestRunner_PY3
+    file = open('./result2.html',mode='wb')
+    runner = HTMLTestRunner_PY3.HTMLTestRunner(stream=file,title='百度搜索测试报告',description='含有三条用例，测试中文、英文、拼音的三个的搜索结果')
+
+    runner.run(suite)
