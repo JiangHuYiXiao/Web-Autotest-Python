9221d91059e971ad0b50340579f69e41727c3627
JiangHuYiXiao <1163270704@qq.com>
2020-04-22 19:21:02 +0800

    unittest框架

--- 第四章-unittest框架/unittest框架_自动发送邮件10/case/TestCase_baidu.py
+++ 第四章-unittest框架/unittest框架_自动发送邮件10/case/TestCase_baidu.py
@@ -4,14 +4,14 @@
 # @Software       : Web-Autotest-Python
 # @Python_verison : 3.7
 
-
+# 给类或者方法加上注释后，在报告中的用例名称后就会有
 # 用例运行的结果有四种状态，{成功:.,错误:E，失败:F，跳过:S}
 import unittest
 from selenium import webdriver
 from time import sleep
 
 class TestCase_baidu(unittest.TestCase):
-
+    '''百度搜索用例集合'''
     @classmethod
     def setUpClass(self):
         self.driver = webdriver.Chrome()
@@ -30,6 +30,7 @@ class TestCase_baidu(unittest.TestCase):
         print('一条用例执行完毕')
 
     def test_baidu_english(self):
+        ''' 英文搜索'''
         self.driver.find_element_by_xpath('//input[@id = "kw"]').send_keys('keep up')
         self.driver.find_element_by_xpath('//input[@id="su"]').click()
         sleep(2)
@@ -37,6 +38,7 @@ class TestCase_baidu(unittest.TestCase):
         self.assertIn('keep up',self.driver.page_source,'不包含')
 
     def test_baidu_chinese(self):
+        ''' 中文搜索'''
         self.driver.find_element_by_xpath('//input[@id = "kw"]').send_keys('鬼吹灯')
         self.driver.find_element_by_xpath('//input[@id="su"]').click()
         sleep(2)
@@ -45,6 +47,7 @@ class TestCase_baidu(unittest.TestCase):
         self.assertIn('沙子',self.driver.page_source,'不包含')
 
     def test_baidu_pinyin(self):
+        ''' 拼音搜索'''
         self.driver.find_element_by_xpath('//input[@id = "kw"]').send_keys('laohu')
         self.driver.find_element_by_xpath('//input[@id="su"]').click()
         sleep(2)
