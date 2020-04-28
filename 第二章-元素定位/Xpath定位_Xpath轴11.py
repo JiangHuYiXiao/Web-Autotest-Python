ca0943d917624ed95da7a3dbbce50613601c58df
JiangHuYiXiao <1163270704@qq.com>
2020-04-28 08:07:25 +0800

    Xpath定位_Xpath轴11

--- 第二章-元素定位/Xpath定位_Xpath轴11.py
+++ 第二章-元素定位/Xpath定位_Xpath轴11.py
@@ -15,5 +15,27 @@
     # //img[@alt='div1']/ancestor::div                  div1的上一层div
 
 # 4、通过text()函数定位
+# 通过text()函数可以定位到元素文本包含某些关键内容的页面元素
     # //a[text()="百度一下"]
     # //a[.="百度一下"]
+    # //a[contains(.,”百度”)]
+    # //a[contains(text(),”百度”)]
+    # //a[contains(text(),”百度”)]/preceding::div
+    # //a[contains(百度”)]/..
+
+
+# 5、descendant：选择当前节点所有下层的节点(子、孙)
+    # //div[@name=‘div2’]/descendant::img
+
+# 6、following：选择当前节点之后显示的所有节点
+    # //div[@id=‘div1’]/following::img
+
+# 7、following-sibling：选择当前节点后续所有兄弟节点
+    # //a[@href=‘http://www.sogou.com’]/following-sibling::input
+
+# 8、preceding：选择当前节点前面的所有节点
+    # //img[@alt=‘div2-img2’]/preceding::div
+
+# 9、Preceding-sibling：选择当前节点前面的所有亲兄弟节点
+    # //input[@value=‘查询’]/preceding-sibling::a[1]
+
