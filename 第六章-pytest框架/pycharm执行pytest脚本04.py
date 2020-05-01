5468c01c6e6cfa8f7c005458777d7045ee1445eb
JiangHuYiXiao <1163270704@qq.com>
2020-05-01 10:32:12 +0800

    pytest框架

---
+++ 第六章-pytest框架/pycharm执行pytest脚本04.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/5/1 10:19
+# @Software       : Web-Autotest-Python
+# @Python_verison : 3.7
+# 之前执行python文件时候，我们在文件中直接右键，然后run
+# 执行unittest脚本时候，我们是run unittest
+# 但是执行pytest脚本时直接执行是不生效的，这个时候，需要我们设置一下编辑器pycharm的关于该文件的默认runner
+
+# 配置路径：setting--->tools--->python integrated tools --->testing--->default tester runner
+# 配置完成后需要在该文件的父目录下右键进行执行，在该文件上执行还是不行的
+
