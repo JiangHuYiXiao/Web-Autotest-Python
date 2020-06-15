62a10967e3636a4de64aaaf557dc60a1e1b1a4b8
JiangHuYiXiao <1163270704@qq.com>
2020-06-15 17:14:39 +0800

    JSON文件读取和数据处理05

--- 第七章-测试框架之数据驱动应用/JSON文件的读取和数据处理05/Test_Json.py
+++ 第七章-测试框架之数据驱动应用/JSON文件的读取和数据处理05/Test_Json.py
@@ -1,33 +1,148 @@
 # -*- coding:utf-8 -*-
-
-
 # 使用json数据文件来进行驱动
 from selenium import webdriver
-import time,unittest,ddt
-
+import time,unittest,ddt,logging,traceback
+from Report_Template import htmlTemplate    # JSON文件的读取和数据处理05
+from selenium.common.exceptions import NoSuchElementException
+# 1、使用全局变量
+'''
 @ddt.ddt            # 声明
-class Test(unittest.TestCase):
-    # @classmethod
-    # def setUpClass(self):
-    #     self.driver = webdriver.Chrome()
-    #     self.driver.maximize_window()
-    #     self.driver.get('http://www.baidu.com')
-    #     self.driver.implicitly_wait(5)
-    # @classmethod
-    # def tearDownClass(self):
-    #     self.driver.quit()
+class Test1(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        # 使用全局变量来解决，table表格数据为空的问题
+        global baidu_test
+        baidu_test = ' '
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(5)
+
+
+    @classmethod
+    def tearDownClass(self):
+        # global baidu_test
+        print(122121)
+        self.driver.quit()
+        htmlTemplate(baidu_test)
+
+    def setUp(self):
+        self.driver.get('http://www.baidu.com')
+
+    def tearDown(self):
+        # global baidu_test
+        print(baidu_test)
+
     @ddt.file_data('data.json')                 # 加载json文件
     @ddt.unpack
     def test_baidu(self,value):
-        print(value)
-        # shiji,yuqi = value.split('||')
-        # print(shiji,yuqi)
-        # self.driver.find_element_by_xpath('//input[@id="su"]').send_keys('')
-        # time.sleep(2)
-        # self.driver.find_element_by_xpath('//input[@id="kw"]').click()
-        # time.sleep(2)
-        # self.assertIn(data,self.driver.page_source,'用例执行不通过')
+        flagDict = {0:'red',1:'green'}
+        try:
+            start = time.time()
+            startTime = time.strftime(" %Y - %M - %d %H:%M:%S", time.localtime())
+            testdata,exceptdata = value.split('||')
+            print(testdata,exceptdata)
+            self.driver.find_element_by_xpath('//input[@id="kw"]').clear()
+            self.driver.find_element_by_xpath('//input[@id="kw"]').send_keys(testdata)
+            time.sleep(2)
+            self.driver.find_element_by_xpath('//input[@id="su"]').click()
+            time.sleep(2)
+            self.assertIn(exceptdata,self.driver.page_source,'用例执行不通过')
+        except NoSuchElementException as e:
+            logging.error("查找的页面元素不存在，异常堆栈信息：" + str(traceback.format_exc()))  # 抛出具体的代码异常的堆栈信息
+            status = 'fail'
+            flag = 0
+        except AssertionError as e:
+            logging.info("搜索:%s,期望:%s,失败" % (testdata,exceptdata))
+            status = 'fail'
+            flag = 0
+        except Exception as e:
+            logging.error('未知错误，信息：' + str(traceback.format_exc()))
+            status = 'fail'
+            flag = 0
+        else:
+            logging.info("搜索:%s,期望:%s,通过" % (testdata, exceptdata))
+            status = 'pass'
+            flag = 1
+        wasteTime = time.time()- start
+        # 使用全局变量来解决，table表格数据为空的问题
+        global baidu_test
+        baidu_test +='''
+            # <tr>
+            # <td>%s</td>
+            # <td>%s</td>
+            # <td>%s</td>
+            # <td>%.2f</td>
+            # <td style="color:%s;">%s</td>
+            # </tr>''' % (testdata,exceptdata,startTime,wasteTime,flagDict[flag],status)
 
+
+# 2、使用类的属性的方式解决table为空
+@ddt.ddt            # 声明
+class Test1(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        # 使用全局变量来解决，table表格数据为空的问题
+
+        Test1.baidu_test = ' '
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(5)
+
+
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
+        htmlTemplate(Test1.baidu_test)
+
+    def setUp(self):
+        self.driver.get('http://www.baidu.com')
+
+    def tearDown(self):
+        # global baidu_test
+        print(Test1.baidu_test)
+
+    @ddt.file_data('data.json')                 # 加载json文件
+    @ddt.unpack
+    def test_baidu(self,value):
+        flagDict = {0:'red',1:'green'}
+        try:
+            start = time.time()
+            startTime = time.strftime(" %Y - %M - %d %H:%M:%S", time.localtime())
+            testdata,exceptdata = value.split('||')
+            print(testdata,exceptdata)
+            self.driver.find_element_by_xpath('//input[@id="kw"]').clear()
+            self.driver.find_element_by_xpath('//input[@id="kw"]').send_keys(testdata)
+            time.sleep(2)
+            self.driver.find_element_by_xpath('//input[@id="su"]').click()
+            time.sleep(2)
+            self.assertIn(exceptdata,self.driver.page_source,'用例执行不通过')
+        except NoSuchElementException as e:
+            logging.error("查找的页面元素不存在，异常堆栈信息：" + str(traceback.format_exc()))  # 抛出具体的代码异常的堆栈信息
+            status = 'fail'
+            flag = 0
+        except AssertionError as e:
+            logging.info("搜索:%s,期望:%s,失败" % (testdata,exceptdata))
+            status = 'fail'
+            flag = 0
+        except Exception as e:
+            logging.error('未知错误，信息：' + str(traceback.format_exc()))
+            status = 'fail'
+            flag = 0
+        else:
+            logging.info("搜索:%s,期望:%s,通过" % (testdata, exceptdata))
+            status = 'pass'
+            flag = 1
+        wasteTime = time.time()- start
+        # 使用全局变量来解决，table表格数据为空的问题
+
+        Test1.baidu_test +='''
+            <tr>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%.2f</td>
+            <td style="color:%s;">%s</td>
+            </tr>''' % (testdata,exceptdata,startTime,wasteTime,flagDict[flag],status)
 
 if __name__ == '__main__':
     unittest.main()
