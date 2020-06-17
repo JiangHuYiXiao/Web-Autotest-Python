fb9b36601c02eae0e3677f0d9218748bb5f235a6
JiangHuYiXiao <1163270704@qq.com>
2020-06-17 09:11:24 +0800

    logging模块应用及用例异常抛出的应用04

--- 第七章-测试框架之数据驱动应用/logging模块应用及用例异常抛出的应用04/test_logging.py
+++ 第七章-测试框架之数据驱动应用/logging模块应用及用例异常抛出的应用04/test_logging.py
@@ -3,6 +3,26 @@
 # @Time           : 2020/6/8 9:06
 # @Software       : Web-Autotest-Python
 # @Python_verison : 3.7
+'''
+各参数信息：
+filename: 指定日志文件名  
+filemode: 和file函数意义相同，指定日志文件的打开模式，'w'或'a'  
+format: 指定输出的格式和内容，format可以输出很多有用信息，如上例所示:  
+ %(levelno)s: 打印日志级别的数值  
+ %(levelname)s: 打印日志级别名称  
+ %(pathname)s: 打印当前执行程序的路径，其实就是sys.argv[0]  
+ %(filename)s: 打印当前执行程序名  
+ %(funcName)s: 打印日志的当前函数
+
+ %(lineno)d: 打印日志的当前行号  
+ %(asctime)s: 打印日志的时间  
+ %(thread)d: 打印线程ID  
+ %(threadName)s: 打印线程名称  
+ %(process)d: 打印进程ID  
+ %(message)s: 打印日志信息  
+datefmt: 指定时间格式，同time.strftime()  
+level: 设置日志级别，默认为logging.WARNING 
+'''
 # 分析：
     # 测试逻辑(WebDriver实例)
     # 打开百度首页
