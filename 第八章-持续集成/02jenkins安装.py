0b05e3432552541d8241b464e4c1e4dad47f851d
JiangHuYiXiao <1163270704@qq.com>
2020-07-21 20:22:43 +0800

    jenkins安装

---
+++ 第八章-持续集成/02jenkins安装.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/7/20 8:49
+# @Software       : Web-Autotest-Python
+# @Python_verison : 3.7
+
+# jenkins安装：
+    # 1、下载jenkins的war，jenkins.war安装包
+    # 2、在jenkins.war文件的路径下执行java -jar jenkins.war
+    # 3、默认端口是8080，访问地址为：http://localhost:8080/
+    # 4、jenkins系统的账号为：jiangtao_jiang   密码：123456
+
