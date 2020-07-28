07ff8dee5cd0bd8b4b79373922c662c1d6369ee9
JiangHuYiXiao <1163270704@qq.com>
2020-07-28 09:28:01 +0800

    整体项目实战

---
+++ 第九章-整体项目实战/02Test_Baidu_Pro/tools/ReadExcel.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+# @Author         : 江湖一笑
+# @Time           : 2020/7/28 8:53
+# @Software       : Web-Autotest-Python
+# @Python_verison : 3.7
+'''
+从excel中读取多个数据，用于用例中，作为数据驱动
+'''
+from openpyxl import load_workbook
+
+class Parse_Excel():
+    def __init__(self,excel_path,sheetname):
+        self.wb = load_workbook(excel_path)
+        self.sheet = self.wb.get_sheet_by_name(sheetname)
+
+    def getDataFromSheet(self):
+        datalist = []
+
+        for line in self.sheet.rows[1:]:
+            tmplist= []
+            tmplist.append(line[0].value)
+            tmplist.append(line[1].value)
+            datalist.append(tmplist)
+        return datalist
+
+
+
+if __name__ == '__main__':
+    excel_path = './baidu_test_data.xlsx'
+    sheetname = 'baidu_sou'
+    pe = Parse_Excel(excel_path,sheetname)
+    print(pe.getDataFromSheet())
